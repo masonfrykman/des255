e5b7fb9609bd8cb4e8dcb12566b3e6d0eceaebb7
masonfrykman <masonfrykman@proton.me>
2025-09-07 14:58:22 -0500

    cairo outputted!

--- project1-DigitalLibs/code/src/main.c
+++ project1-DigitalLibs/code/src/main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <cairo.h>
 
-int main(int argc, char* argv[]) {	
+#define IMAGE_WIDTH_PIXELS 11 * 300
+#define IMAGE_HEIGHT_PIXELS 17 * 300
+
+int main(int argc, char* argv[]) {
+
+    if(argc < 2) {
+        printf("Include a path to output to as the first argument.\n");
+        return 2;
+    }
+
+    // Create a surface to write to
+    cairo_surface_t *surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, IMAGE_WIDTH_PIXELS, IMAGE_HEIGHT_PIXELS);
+    cairo_t *c = cairo_create(surface);
+
+    // Write the surface to final PNG
+    // TODO: check this path is accessible
+    cairo_status_t writeOperation = cairo_surface_write_to_png(surface, argv[1]);
+    // TODO: error checking
+
 	return 0;
 }

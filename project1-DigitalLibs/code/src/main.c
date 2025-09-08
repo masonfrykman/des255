c2f4dca5b24e3eb684e51852fe77ab1c39bfe04d
masonfrykman <masonfrykman@proton.me>
2025-09-07 19:58:58 -0500

    SDL main whatever

--- project1-DigitalLibs/code/src/main.c
+++ project1-DigitalLibs/code/src/main.c
@@ -1,5 +1,18 @@
-#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main(int argc, char* argv[]) {	
-	return 0;
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppEvent(void** appstate, SDL_Event* event) {
+	return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void* appstate) {
+	return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void* appstate, SDL_AppResult result) {
+	return;
 }

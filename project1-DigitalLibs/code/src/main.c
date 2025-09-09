41ec7847ea9d17a7e5ff380e30d3c7c22e4634ea
masonfrykman <masonfrykman@proton.me>
2025-09-08 21:45:58 -0500

    we got the DESIRED OUTPUT TEAM, almost done

--- project1-DigitalLibs/code/src/main.c
+++ project1-DigitalLibs/code/src/main.c
@@ -8,15 +8,16 @@
 
 #include "HumanEye.h"
 
-static HumanEye** windows;
-int numWindows = 0;
+static HumanEye* windows[P1_MAX_EYES];
+static int eyeCount = 0;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
+	// App Setup
     SDL_SetAppMetadata("DES255 Project 1", "1.0.0", "dev.frykman.des255-p1");
 
 	if(argc < 2) {
 		printf("include a directory of png eyes as the argument\n");
-		exit(2);
+		exit(1);
 	}
     
     if(!SDL_Init(SDL_INIT_VIDEO)) {
@@ -24,8 +25,44 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
         return SDL_APP_FAILURE;
     }
 
-	// TODO: load in the list of eyes
-	newEye("../eyes/left1.png");
+	for(size_t i = 0; i < P1_MAX_EYES; i++) {
+		windows[i] = NULL;
+	}
+
+	// Load in the list of eyes,
+	//		For each eye, make a window containing said eye.
+	char directory[2048];
+	strncpy(directory, argv[1], 2047);
+	strcat(directory, "list-of-eyes.txt");
+
+	FILE* eyesList = fopen(directory, "r");
+	if(eyesList == NULL) {
+		printf("couldn't open the list of eyes.\n");
+		exit(1);
+	}
+
+	char* line;
+	size_t lineLength;
+	while(eyeCount != P1_MAX_EYES && getline(&line, &lineLength, eyesList) != -1) {
+		if(line[strlen(line) - 1] == '\n') { // clean the string end (it breaks newEye)
+			line[strlen(line) - 1] = '\0';
+		}
+
+		// create the path of the image
+		char nD[2048];
+		strncpy(nD, argv[1], 2047);
+		strcat(nD, line);
+
+		// Make the eye window
+		HumanEye* candidate = newEye(nD);
+		if(candidate == NULL) continue;
+
+		// Append to the list of windows for cleanup later
+		windows[eyeCount] = candidate;
+		eyeCount++;
+	}
+
+	printf("done loading %d eyes.\n", eyeCount);
 /*
     if(!SDL_CreateWindowAndRenderer("HELLO", 800, 600, 0, &leftwindow, &leftrenderer)) {
         SDL_Log("SDL window creation failed! %s", SDL_GetError());
@@ -52,5 +89,9 @@ SDL_AppResult SDL_AppIterate(void* appstate) {
 }
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) {
-	return;
+	// Cleanup the eyes
+	for(int i = 0; i < eyeCount; i++) {
+		SDL_DestroyWindow(windows[i]->window);
+		free(windows[i]);
+	}
 }

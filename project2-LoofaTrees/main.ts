a46dc6d4c30bf401ba58eabbd85c8177f2a55524
masonfrykman <masonfrykman@proton.me>
2025-10-16 11:27:50 -0500

    better resize

--- project2-LoofaTrees/main.ts
+++ project2-LoofaTrees/main.ts
@@ -9,12 +9,7 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
 
-function updateCameraPos() {
-    camera.position.x = 0;
-    camera.position.z = 20 - (20 / window.innerWidth);
-    camera.position.y = 4;
-}
-updateCameraPos()
+
 
 const renderer = new THREE.WebGLRenderer({antialias: true, preserveDrawingBuffer: true, alpha: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,7 +23,14 @@ controls.enableZoom = true;
 controls.enablePan = false;
 controls.minDistance = 5;
 controls.maxDistance = 8;
-controls.target = new THREE.Vector3(0, camera.position.y / 2, 0)
+
+function updateCameraPos() {
+    camera.position.x = 0;
+    camera.position.z = 20 - (20 / window.innerWidth);
+    camera.position.y = 4;
+    controls.target = new THREE.Vector3(0, camera.position.y / 2, 0)
+}
+updateCameraPos()
 
 const skyColor = 0x37a9fc
 const groundColor = 0xaaffaa
